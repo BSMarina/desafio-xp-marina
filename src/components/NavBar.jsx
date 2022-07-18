19799bfe695a0ca9258f5b94117c47746e1e0f23
Marina Barros <marinabarrossilva@gmail.com>
2022-07-18 13:38:22 -0300

    Feat(NavBar): adicionada a lógica de navegação entre as abas carteira e mercado

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function NavBar() {
+    const navigate = useNavigate();
     return (
         <ul>
             <li>
-                <button>Minha Carteira</button>
+                <button onClick={ () => navigate('/carteira') }>Minha Carteira</button>
             </li>
             <li>
-                <button>Mercado</button>
+                <button onClick={ () => navigate('/mercado') }>Mercado</button>
             </li>
         </ul>
     );
