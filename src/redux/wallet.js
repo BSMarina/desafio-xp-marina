9b6392219308a8cc984f12a6749780502e967897
Marina Barros <marinabarrossilva@gmail.com>
2022-07-22 02:49:55 -0300

    Feat: criada a action para manipular as transações de saque e deposito

--- src/redux/wallet.js
+++ src/redux/wallet.js
@@ -2,18 +2,14 @@ import { createSlice } from '@reduxjs/toolkit'
 
 export const walletSlice = createSlice({
   name: 'wallet',
-  initialState: {userStocks: []}, //definir estado inicial depois
+  initialState: {userStocks: [], userAccount: 0}, //definir estado inicial depois
   reducers: {
-    sell: (state) => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
-      state.value += 1 //implementar ação de venda
-    },
     buySell: (state, action) => {
-      return state = {...state, userStocks: action.payload}
+      state.userStocks = action.payload
     },
+    updateAccount: (state, action) => {
+      state.userAccount = action.payload
+    }
   },
 })
 
