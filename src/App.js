ddf0e33ca7b5173c54073ed326a4c904546e24b5
Marina Barros <marinabarrossilva@gmail.com>
2022-07-23 15:08:57 -0300

    Feat: adicionada a configuração do App para receber o styled components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import './App.css';
 import { Route, Routes } from 'react-router-dom';
+
 import Login from './pages/Login';
 import Wallet from './pages/Wallet';
 import Market from './pages/Market';
@@ -10,18 +10,26 @@ import Withdraw from './pages/Withdraw';
 import Buy from './pages/Buy';
 import Sell from './pages/Sell';
 
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from './assets/styles/global';
+import defaultTheme from './assets/styles/themes/default';
+
 function App() {
   return (
-    <Routes>
-      <Route exact path='/' element={ <Login/> }/>
-      <Route exact path='/carteira' element={ <Wallet/> }/>
-      <Route exact path='/mercado' element={ <Market/> }/>
-      <Route exact path='/deposito' element={ <Deposit/> }/>
-      <Route exact path='/saque' element={ <Withdraw/> }/>
-      <Route exact path='/compra' element={ <Buy/> }/>
-      <Route exact path='/venda' element={ <Sell/> }/>
-      <Route path='*' element={ <NotFound/> }/>
-    </Routes>
+    <ThemeProvider theme={ defaultTheme }>
+      <Routes>
+        <Route exact path='/' element={ <Login/> }/>
+        <Route exact path='/carteira' element={ <Wallet/> }/>
+        <Route exact path='/mercado' element={ <Market/> }/>
+        <Route exact path='/deposito' element={ <Deposit/> }/>
+        <Route exact path='/saque' element={ <Withdraw/> }/>
+        <Route exact path='/compra' element={ <Buy/> }/>
+        <Route exact path='/venda' element={ <Sell/> }/>
+        <Route path='*' element={ <NotFound/> }/>
+      </Routes>
+      <GlobalStyles />
+    </ThemeProvider>
+
     );
 }
 
