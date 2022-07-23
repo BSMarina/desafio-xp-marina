0c943887570d7be143db954abfd1bb6705b98e8b
Marina Barros <marinabarrossilva@gmail.com>
2022-07-23 16:26:19 -0300

    Feat: criado o arquivo para guardar o esquema de cores

---
+++ src/assets/styles/themes/default.js
@@ -0,0 +1,15 @@
+const defaultTheme = {
+    colors: {
+        background: {
+            main: '#000000',
+            alternative: '#CCCCCC',
+        },
+        elements: {
+            main: '#FBC105',
+            detail: '#54D088',
+            white: '#FFFFFF'
+        },
+    },
+};
+
+export default defaultTheme;
