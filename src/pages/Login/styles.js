8239dc5b343ff7f89ceb173bc0f83001c1b18d47
Marina Barros <marinabarrossilva@gmail.com>
2022-07-23 18:41:56 -0300

    Chore: pasta images colocada em assets

--- src/pages/Login/styles.js
+++ src/pages/Login/styles.js
@@ -1,2 +1,13 @@
 import styled from 'styled-components';
 
+export const Container = styled.div`
+    background-color: aliceblue;
+`;
+
+export const ImgContent = styled.div`
+    background-color: aliceblue;
+`;
+
+export const FormContent = styled.div`
+    background-color: aliceblue;
+`;
