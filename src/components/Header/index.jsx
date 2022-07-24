d77fec9f0cfcf227c27662c94f574a1a0b769f95
Marina Barros <marinabarrossilva@gmail.com>
2022-07-23 21:28:17 -0300

    Feat: estilo do content

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux';
 import { useLocation, useNavigate } from 'react-router-dom';
 
 import iconArrowBack from '../../assets/images/iconArrowBack.svg';
-import Container from './styles'
+import * as SC from './styles'
 
 export default function Header() {
   const { pathname } = useLocation();
@@ -23,14 +23,16 @@ export default function Header() {
   const userEmail = useSelector((state) => state.user.email)
 
   return (
-      <Container>
+      <SC.Container>
         <button onClick={ () => navigate(-1) }>
           <img src={ iconArrowBack } alt="Ícone de seta para voltar" />
         </button>
+        <SC.UserContent>
         <h2>{ userEmail }</h2> 
         <button onClick={ handleTransaction } value={ btnInfo }>
           { btnInfo }
         </button>
-      </Container>
+        </SC.UserContent>
+      </SC.Container>
   );
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,7 +1,13 @@
 import styled from 'styled-components';
 
 const Container = styled.header`
-    background-color: aliceblue;
+    background-color: ${({theme}) => theme.colors.background.alternative};
+    color: ${({theme}) => theme.colors.elements.white};
+    display: flex;
+    width: 100vw;
+    justify-content: space-around;
+    font-size: 8px;
+    height: 50px;
 `;
 
 export default Container;
