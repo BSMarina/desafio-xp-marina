6ce5806edeebde0a7232afbd46a57a31d69a1bcd
Marina Barros <marinabarrossilva@gmail.com>
2022-07-23 21:41:08 -0300

    feat: estilo do usercontent

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,13 +1,31 @@
 import styled from 'styled-components';
 
-const Container = styled.header`
+export const Container = styled.header`
     background-color: ${({theme}) => theme.colors.background.alternative};
     color: ${({theme}) => theme.colors.elements.white};
     display: flex;
     width: 100vw;
-    justify-content: space-around;
-    font-size: 8px;
+    justify-content: space-between;
     height: 50px;
+    padding: 8px;
 `;
 
-export default Container;
+export const UserContent = styled.div`
+    color: ${({theme}) => theme.colors.elements.white};
+    display: flex;
+    font-size: 8px;
+    align-items: center;
+    column-gap: 8px;
+
+    button {
+        background-color: ${({theme}) => theme.colors.elements.main};
+        border-radius: 5px;
+        border-style: none;
+        font-size: 16px;
+        padding: 5px;
+        
+        :active {
+            background-color: ${({theme}) => theme.colors.elements.main}70;
+        }
+    }
+`;
