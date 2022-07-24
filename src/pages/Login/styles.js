129b977e80946ceab033c839efd62192339edb2e
Marina Barros <marinabarrossilva@gmail.com>
2022-07-24 01:16:30 -0300

    Fix: estilo do form no login

--- src/pages/Login/styles.js
+++ src/pages/Login/styles.js
@@ -4,10 +4,11 @@ export const Container = styled.div`
     align-items: center;
     display: flex;
     flex-direction: column;
-    height: 100vh;
+    min-height: 100vh;
     justify-content: space-around;
-    max-width: 1980px;
-    width: 100vw;
+    /* max-width: 1980px;
+    width: 100vw; */
+    padding: 0 16px;
 `;
 
 export const ImgContent = styled.div`
@@ -23,12 +24,12 @@ export const FormContent = styled.form`
     flex-direction: column;
     margin-bottom: 20px;
     row-gap: 14px;
-    width: 50%;
+    width: 100%;
     
     input {
         background-color: transparent;
         border-bottom: 2px solid ${({theme}) => theme.colors.elements.white};
-        border-radius: 5px;
+        /* border-radius: 5px; */
         border-style: none none solid none;
         box-shadow: none;
         color: ${({theme}) => theme.colors.elements.main};
@@ -37,11 +38,11 @@ export const FormContent = styled.form`
         outline: none;
         padding-left: 10px;
         transition: all 0.4s ease-in-out;
-        width: 40%;
+        width: 100%;
         
         :hover, :focus {
-            background-color: ${({theme}) => theme.colors.background.alternative}60;
-            border: 2px solid ${({theme}) => theme.colors.elements.white};
+            /* background-color: ${({theme}) => theme.colors.background.alternative}60; */
+            border-bottom: 2px solid ${({theme}) => theme.colors.elements.main};
         }
     }
 
@@ -52,7 +53,7 @@ export const FormContent = styled.form`
         font-size: 16px;
         height: 36px;
         margin-top: 10px;
-        width: 40%;
+        width: 100%;
         
         :active {
             background-color: ${({theme}) => theme.colors.elements.main}70;
